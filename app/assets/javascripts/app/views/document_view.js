374cb436290fdcf6496560ffe9ab597923e13b53
smrutiparida <smruti.parida@gmail.com>
2013-11-29 17:01:21 +0530

    document uploading javascript

--- app/assets/javascripts/app/views/document_view.js
+++ app/assets/javascripts/app/views/document_view.js
@@ -4,6 +4,20 @@ app.views.Document = app.views.Base.extend({
 
   className : "document loaded",
 
+  tagName: "div",
+
+  className: 'hoverable',
+
+  events: {
+    'click .icons-check_yes_ok+a': 'toggleShowDeleteButton'
+  },
+
+  showDelete: function(evt) {
+    if (evt) { evt.preventDefault(); };
+    this.model.toggleSelected();
+    this.$el.find('.icons-check_yes_ok').toggleClass('selected');
+    app.router.aspects_stream();
+  },
   initialize : function() {
     $(this.el).attr("id", this.model.get("guid"));
     this.model.bind('remove', this.remove, this);
