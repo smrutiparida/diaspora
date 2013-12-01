6d648a8bb4a4b16e601e75b8db988e3e98d775bf
smrutiparida <smruti.parida@gmail.com>
2013-12-01 12:33:41 +0530

    attempt 2 to show documents uploaded

--- app/assets/javascripts/app/views/content_view.js
+++ app/assets/javascripts/app/views/content_view.js
@@ -8,9 +8,8 @@ app.views.Content = app.views.Base.extend({
       text : app.helpers.textFormatter(this.model.get("text"), this.model),
       largePhoto : this.largePhoto(),
       smallPhotos : this.smallPhotos(),
-      location: this.location()
-      //,
-      //documents: this.documents()
+      location: this.location(),
+      documents: this.documents()
     });
   },
 
