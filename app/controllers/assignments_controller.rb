426db0064b5fdf6b1775c20406f788cfbd111bec
smrutiparida <smruti.parida@gmail.com>
2013-12-02 18:00:57 +0530

    Create assignment

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,7 +1,7 @@
 class AssignmentsController < ApplicationController
 
   before_filter :authenticate_user!, :only => [:new, :create]
-  respond_to :html, :json
+  respond_to :html, :json, :js
 
   def new
     
@@ -15,16 +15,23 @@ class AssignmentsController < ApplicationController
   def create
     @assignment = current_user.build_post(:assignment, assignment_params)
     if @assignment.save      
+      #@response = {}
+      @response = @assignment.as_api_response(:backbone)
+      @response[:success] = true
+      @response[:message] = "Assignment created successfully."
       respond_to do |format|
-        format.json { render :json => {"success" => true, "data" => @assignment.as_api_response(:backbone)} }        
-      end
-    else
-      flash[:error] = "Assignment creation failed. Please try again!"
-      respond_to do |format|
-        format.html do
-          render :layout => false
-        end    
-      end  
+      format.js
+    
+  #    respond_to do |format|
+  #      format.json { render :json => {"success" => true, "data" => @assignment.as_api_response(:backbone)} }        
+  #    end
+  #  else
+  #    flash[:error] = "Assignment creation failed. Please try again!"
+  #    respond_to do |format|
+  #      format.html do
+  #        render :layout => false
+  #      end    
+  #    end  
     end
   end  
 
