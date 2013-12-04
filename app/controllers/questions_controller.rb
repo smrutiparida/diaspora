0d0ee43fc118915afd99382fe631ec0e281adc66
smrutiparida <smruti.parida@gmail.com>
2013-12-04 11:06:21 +0530

    Create Quiz and question bank

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -41,6 +41,6 @@ class QuestionsController < ApplicationController
   end  
 
   def question_params
-    params.require(:question).permit(:description,:type,:answer,:answer1, :answer2,:answer3,:answer4, :tags)
+    params.require(:question).permit(:description,:qtype,:answer,:answer1, :answer2,:answer3,:answer4, :tags)
   end
 end
