7e54945d55036fd92f462507d21d8c5b87d8b400
smrutiparida <smruti.parida@gmail.com>
2013-12-08 10:54:51 +0530

    Library page beautification

---
+++ app/controllers/library_controller.rb
@@ -0,0 +1,15 @@
+class LibraryController < ApplicationController
+
+  before_filter :authenticate_user!, :only => [:index]
+  respond_to :html, :json, :js
+
+  def index
+    @documents = Document.where(:diaspora_handle => current_user.diaspora_handle).order(:updated_at)  
+    @assignments = Assignment.where(:diaspora_handle => current_user.diaspora_handle).order(:updated_at)  
+    @quizzes = Quiz.where(:diaspora_handle => current_user.diaspora_handle).order(:updated_at)  
+        
+    respond_to do |format|
+      format.html
+      format.json {render :json => {'documents' => @documents, 'assignments' => @assignments, 'quizzes' => @quizzes}.to_json}
+    end
+  end
