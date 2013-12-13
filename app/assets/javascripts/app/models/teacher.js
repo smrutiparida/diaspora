961e8151eedef437116f8d6f5706847cb7d7bda4
smrutiparida <smruti.parida@gmail.com>
2013-12-13 19:44:29 +0530

    beautifying the aspects list

--- app/assets/javascripts/app/models/teacher.js
+++ app/assets/javascripts/app/models/teacher.js
@@ -1,11 +1,11 @@
 app.models.Teacher = Backbone.Model.extend({
   urlRoot : "/aspects/teacher/"
-},
+},{
 
   get_teacher : function(ids) {
     var teacher = new app.models.Teacher();
     teacher.deferred = teacher.fetch({url : teacher.urlRoot + ids[0]})
-    return teacher  
+    return teacher;  
   }
 
 
