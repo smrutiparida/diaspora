5ea36fa4a8fea3fa99310da6c3aaad7735cd6a2b
smrutiparida <smruti.parida@gmail.com>
2013-12-13 22:38:55 +0530

    beautifying the aspects list

--- app/assets/javascripts/app/views/teacher_view.js
+++ app/assets/javascripts/app/views/teacher_view.js
@@ -1,9 +1,3 @@
 app.views.Teacher = app.views.Base.extend({
-  templateName: "teacher",
-
-  presenter : function() {
-    return _.extend(this.defaultPresenter(), {
-      teacher : this.model.attributes
-    })
-  }
+  templateName: "teacher"
 });
