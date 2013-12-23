5a9eed2d0c4fd381a6b99e05424a71bf41243f97
unknown <smruti.parida@gmail.com>
2013-12-23 10:17:31 +0530

    assignment bug fix

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -4,25 +4,31 @@ class LibraryController < ApplicationController
   respond_to :html, :json, :js
 
   def index
+    #role = Roles.where(:person_id => current_user.person.id, :name => 'teacher').first
+    #@teacher = role.nil? ? false : true
     @assignment_active = (params[:tab] == "assignment") ? 1 : 0
+    
     @documents = Document.where(:diaspora_handle => current_user.diaspora_handle).order(:updated_at)  
-    #@assignments = Assignment.where(:diaspora_handle => current_user.diaspora_handle).order(:updated_at)  
-    #@quizzes = Quiz.where(:diaspora_handle => current_user.diaspora_handle).order(:updated_at)  
-           
     
     #below logic valid for students. For teachers the logic will be different.
-    if params[:a_id]
-      all_my_posts = Post.joins(:aspect_visibilities).where(
-          :aspect_visibilities => {:aspect_id => params[:a_id]})
+    if @teacher
+      @assignments = Assignment.where(:diaspora_handle => current_user.diaspora_handle).order(:updated_at)  
+      @quizzes = Quiz.where(:diaspora_handle => current_user.diaspora_handle).order(:updated_at)  
     else
-      all_my_posts = Post.joins(:aspect_visibilities).where(
-          :aspect_visibilities => {:aspect_id => current_user.aspect_ids})  
-    end  
-    all_my_post_guid = all_my_posts.map{|a| a.guid}
-    
-    @assignments = Assignment.where(:status_message_guid => all_my_post_guid)
-    @quizzes = Quiz.where(:status_message_guid => all_my_post_guid)
-    @documents += Document.where(:status_message_guid => all_my_post_guid)
+      if params[:a_id]
+        all_my_posts = Post.joins(:aspect_visibilities).where(
+            :aspect_visibilities => {:aspect_id => params[:a_id]})
+      else
+        all_my_posts = Post.joins(:aspect_visibilities).where(
+            :aspect_visibilities => {:aspect_id => current_user.aspect_ids})  
+      end
+
+      all_my_post_guid = all_my_posts.map{|a| a.guid}
+      
+      @assignments = Assignment.where(:status_message_guid => all_my_post_guid)
+      @quizzes = Quiz.where(:status_message_guid => all_my_post_guid)
+      @documents += Document.where(:status_message_guid => all_my_post_guid)
+    end
 
     respond_to do |format|
       format.html
