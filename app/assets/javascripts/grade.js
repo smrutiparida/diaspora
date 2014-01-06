b117fb9136a93e881e78ab04ca69b6bad4369e72
unknown <smruti.parida@gmail.com>
2014-01-06 20:33:10 +0530

    showing grades

--- app/assets/javascripts/grade.js
+++ app/assets/javascripts/grade.js
@@ -60,11 +60,11 @@ function createUploader(){
       $('#file-upload').removeClass("loading");
       if ( responseJSON.success = 'success')
       {
-         
+          alert(responseJSON.students);
           var data2 = new google.visualization.arrayToDataTable(responseJSON.students);
           var table = new google.visualization.Table(document.getElementById('table_div'));
           table.draw(data2, {showRowNumber: true});
-          alert(responseJSON.students);
+
           $('#grades_points').val(responseJSON.students);
       }
       else
