b0c67513035a6714071bfa9ddba5d5c6937b013a
unknown <smruti.parida@gmail.com>
2014-01-07 22:25:00 +0530

    adding links to the course

--- app/controllers/o_embed_caches_controller.rb
+++ app/controllers/o_embed_caches_controller.rb
@@ -6,7 +6,7 @@ class OEmbedCachesController < ApplicationController
   def new
     @modules = Content.where(:aspect_id => params[:a_id])      
     respond_to do |format|
-      format.html { :layout => false }
+      format.html {render :layout => false}
     end
   end
 
