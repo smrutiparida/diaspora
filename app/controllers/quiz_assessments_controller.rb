281a38559875695ca386b0ebbc3a30504fd83746
unknown <smruti.parida@gmail.com>
2014-01-10 03:43:21 +0530

    quiz assessment files

--- app/controllers/quiz_assessments_controller.rb
+++ app/controllers/quiz_assessments_controller.rb
@@ -108,8 +108,8 @@ class QuizAssessmentsController < ApplicationController
     
     @quiz_assessment.quiz_questions_assessments.each do |quiz_answer|
       answer_set = question_hash[quiz_answer.quiz_question_id]
-      if answer_set[:answer].downcase == quiz_answer.answer 
-        quiz_answer.marks = answer_set[:mark].to_i 
+      if answer_set["answer"].downcase == quiz_answer.answer 
+        quiz_answer.marks = answer_set["mark"].to_i 
         @quiz_assessment.marks_obtained += quiz_answer.marks
       else
         quiz_answer.marks = 0
