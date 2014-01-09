088bec6c5f73f82b903ad0a7ea0047c8949350b3
unknown <smruti.parida@gmail.com>
2014-01-10 03:28:53 +0530

    quiz assessment files

--- app/models/quiz_assessment.rb
+++ app/models/quiz_assessment.rb
@@ -2,6 +2,7 @@ class QuizAssessment < ActiveRecord::Base
   
   belongs_to :quiz_assignment, :foreign_key => :quiz_assignment_id, :primary_key => :id
   has_many :quiz_questions_assessments, :foreign_key => :quiz_assessment_id, :dependent => :destroy
+  accepts_nested_attributes_for :quiz_questions_assessments
   
   def self.diaspora_initialize(params = {})
     quiz_assessment = self.new #params.to_hash.slice(:name, :description, :submission_date)
