5e5c25345f7c7c04d4104faa3f6134d523eaa9af
unknown <smruti.parida@gmail.com>
2014-01-13 19:19:07 +0530

    teacher creating a course

--- app/helpers/invitation_codes_helper.rb
+++ app/helpers/invitation_codes_helper.rb
@@ -15,9 +15,27 @@ module InvitationCodesHelper
      
       invitation_details = Invitation.where(:sender_id => inviter.id, :identifier => current_user.email).first
       unless invitation_details.blank?
+      
         @aspect = Aspect.find(invitation_details.aspect_id) 
+      
         inviter.share_with(current_user.person, @aspect)
-     
+        
+        new_aspect = self.aspects.create(:name => @aspect.name, :folder => "Classroom")
+        
+        @inviter_aspect = inviter.aspects.find(invitation_details.aspect_id)
+        contacts_in_aspect = @inviter_aspect.contacts.includes(:aspect_memberships).all    
+        person_in_contacts = Person.where(:id => contacts_in_aspect)
+        user_in_contacts = User.where(:id => contacts_in_aspect)
+        
+        person_in_contacts.each do |existing_member|
+          self.share_with(existing_member, new_aspect)
+        end 
+
+        user_in_contacts.each do |existing_user|
+          user_aspect = existing_user.aspects.where(:name => @aspect.name).first
+          existing_user.share_with(current_user.person, user_aspect)
+        end
+      
         render :partial => 'aspects/add_contact_course', :locals => {:inviter => inviter.person, :aspect => @aspect}
       end  
     end
