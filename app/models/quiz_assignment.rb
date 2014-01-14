4e0094c8629a743f6930f6c5e87b14032e43896f
unknown <smruti.parida@gmail.com>
2014-01-14 08:50:47 +0530

    teacher creating a course

--- app/models/quiz_assignment.rb
+++ app/models/quiz_assignment.rb
@@ -6,13 +6,23 @@ class QuizAssignment < ActiveRecord::Base
     t.add :id    
     t.add :submission_date
     t.add :title
-    t.add :total_marks             
+    t.add :total_marks
+    t.add lambda { |quiz_assignment|
+            quiz_assignment.subdate(quiz_assignment.submission_date)
+          }, :as => :sub_date             
   end 
 
   belongs_to :quiz, :foreign_key => :quiz_id, :primary_key => :id
   delegate :title, :total_marks, to: :quiz
   belongs_to :status_message, :foreign_key => :status_message_guid  , :primary_key => :guid
   validates_associated :status_message
+
+  def subdate(date = nil)    
+    if date
+      Rails.logger.info(date.to_s)
+      DateTime.parse(date.to_s).strftime("%b %d")
+    end
+  end
 
   def self.diaspora_initialize(params = {})
     Rails.logger.info("Model Enter")
