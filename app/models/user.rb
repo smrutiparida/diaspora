28f36932371e4cd1be8ee151aebd160225a54c33
unknown <smruti.parida@gmail.com>
2014-01-20 18:38:41 +0530

    bug fizes, introducing invitaiton link

--- app/models/user.rb
+++ app/models/user.rb
@@ -384,10 +384,10 @@ class User < ActiveRecord::Base
     #self.aspects.create(:name => "Mathematics", :folder => "Classroom")
     #self.aspects.create(:name => "Science", :folder => "Classroom")
     #self.aspects.create(:name => "Social Science", :folder => "Classroom")
-    self.aspects.create(:name => "Sports")
-    self.aspects.create(:name => "Music")
-    self.aspects.create(:name => "Dramatics")
-    self.aspects.create(:name => "Computer")
+    self.aspects.create(:name => "Friends")
+    self.aspects.create(:name => "Close Friends")
+    self.aspects.create(:name => "Classmates")
+    self.aspects.create(:name => "Acquaintances")
 
     #aq = self.aspects.create(:name => I18n.t('aspects.seed.acquaintances'))
 
