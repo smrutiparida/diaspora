ff71f1863008724bf092679258d9677f207a4608
unknown <smruti.parida@gmail.com>
2014-01-24 13:09:06 +0530

    integrating with issuu

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,5 +1,6 @@
 module DocumentsHelper
   API_KEY = "wo753emrs8l2o0vesjaoxti5n3r9cyvi" 
+  API_SECRET = "rmu2dklxoz5htmul2gkhpk6j40qrqx0d"
  
   def create_view(document)
     params = doc_upload_params(document)
@@ -9,7 +10,7 @@ module DocumentsHelper
   end
   
   private
-  
+
   def upload_issuu(params)   
     require "uri"
     require "net/http"
@@ -20,7 +21,6 @@ module DocumentsHelper
   end
 
   def generate_signature(params)
-    API_SECRET = "rmu2dklxoz5htmul2gkhpk6j40qrqx0d"
     string_to_sign = "#{API_SECRET}#{params.sort_by {|k| k.to_s }.to_s}"
     Digest::MD5.hexdigest(string_to_sign)
   end
