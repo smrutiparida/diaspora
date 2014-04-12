1a4d24172fc0dbf34075ca5d91301bd40b2ce855
unknown <smruti.parida@gmail.com>
2014-04-12 08:52:40 +0530

    PZ-Making anonymous post

--- app/assets/javascripts/app/helpers/handlebars-helpers.js
+++ app/assets/javascripts/app/helpers/handlebars-helpers.js
@@ -50,6 +50,8 @@ Handlebars.registerHelper('localTime', function(timestamp) {
 Handlebars.registerHelper('anonymityString', function(author) {
   /* we return here if person.avatar is blank, because this happens when a
    * user is unauthenticated.  we don't know why this happens... */
+  if( _.isUndefined(author.person.avatar) ) { return }
+  
   if(author.name == "Anonymous"){
     htmlStr = Handlebars.helpers['personImage'].call(this, "small", "small");
     return _.template('<span class="img"> <%= htmlStr %> </span>')    
