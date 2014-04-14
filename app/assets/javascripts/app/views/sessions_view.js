6908d70380cacdd0ddae3730145ea6bac3164813
unknown <smruti.parida@gmail.com>
2014-04-14 09:24:21 +0530

    PZ-Adding a course to a running session

--- app/assets/javascripts/app/views/sessions_view.js
+++ app/assets/javascripts/app/views/sessions_view.js
@@ -7,9 +7,9 @@ app.views.Sessions = app.views.Base.extend({
   tagName : "li",
 
   presenter : function() {
-    console.log(this.options.sessions.content);
+    console.log(this.options.sessions);
     return _.extend(this.defaultPresenter(), {
-      sessions : this.options.sessions.content
+      sessions : this.options.sessions
     })
     
   }
