358beea4b8ba6346df6e80c1a04ab885c188f4bc
unknown <smruti.parida@gmail.com>
2014-04-23 16:14:05 +0530

    PZ:making session names clickable

---
+++ app/assets/javascripts/app/views/session_view.js
@@ -0,0 +1,24 @@
+app.views.Session = app.views.Base.extend({
+  templateName: "session",
+
+  tagName: "li",
+
+  events: {
+    'click session-names': 'filterBySession'
+  },
+
+  filterBySession: function(evt) {
+    if (evt) { evt.preventDefault(); };
+    //this.model.toggleSelected();    
+    //$('.all_aspects').find('.icons-check_yes_ok').removeClass('selected');
+    //this.$el.find('.icons-check_yes_ok').addClass('selected');
+    alert("called");
+    app.router.aspects_stream();
+  },
+
+  presenter : function() {
+    return _.extend(this.defaultPresenter(), {
+      session : this.model
+    })
+  }
+});
