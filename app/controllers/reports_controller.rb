1e6137c441684c62b006c98410147a143083d35b
unknown <smruti.parida@gmail.com>
2014-05-29 00:24:46 +0530

    introducing reports contorller for analytics

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -15,33 +15,33 @@ class ReportsController < ApplicationController
     respond_to do |format|
 	  format.html
 	end 
-	@aspect_assessments = AspectAssessment.find(params[:a_id])
+	##@aspect_assessments = AspectAssessment.find(params[:a_id])
 	#data for the aspect id, rank all memebers based on data in aspect_assessments table
 	#columns needed -> aspect_id, person_id, qs_asked, qs_answered, answsers_marked_right, score, answers_resolved
-	@data_for_score_table = []
-	@status_assessment_table = StatusAssessment.find(params[:a_id])
+	##@data_for_score_table = []
+	##@status_assessment_table = StatusAssessment.find(params[:a_id])
 	#have resolved_Date and closed_date added to the POST table
 	#aspect_id, total_respolved, total_marked_right, total_open, total_posts, total_anonymous
-	@data_for_open_conversations = []
-	@data_for_closed_conversations = []
-	@data_for_anonymous_questions = []
+	##@data_for_open_conversations = []
+	##@data_for_closed_conversations = []
+	##@data_for_anonymous_questions = []
 
-	if current_user.role == "teacher"
-	  @assignment_assessments = AssignmentAssessment.where(:assignment_id => @assignment.id)
-	  unless @assignment_assessments.nil?
-	    @assignment_assessments.each do |c|
-	      c.is_checked ? @data2.push([c.diaspora_handle, c.points]) : next
-	      @key = c.points.to_s + " marks"
-	      @temp.has_key?(@key) ? @temp[@key] = @temp[@key] + 1 : @temp[@key] = 1          
-	    end
-	  end    
-	end
+	##if current_user.role == "teacher"
+	##  @assignment_assessments = AssignmentAssessment.where(:assignment_id => @assignment.id)
+	##  unless @assignment_assessments.nil?
+	##    @assignment_assessments.each do |c|
+	##      c.is_checked ? @data2.push([c.diaspora_handle, c.points]) : next
+	##      @key = c.points.to_s + " marks"
+	##      @temp.has_key?(@key) ? @temp[@key] = @temp[@key] + 1 : @temp[@key] = 1          
+	##    end
+	##  end    
+	##end
 
-	@temp.each { |key,value| @data.push([key, value])}
-	Rails.logger.info(@data.to_json)
-	Rails.logger.info(@data2.to_json)
-	#respond_to do |format|
-	#  format.js
-	#end
+	##@temp.each { |key,value| @data.push([key, value])}
+	##Rails.logger.info(@data.to_json)
+	##Rails.logger.info(@data2.to_json)
+	##respond_to do |format|
+	##  format.js
+	##end
   end  
 end 
