0fc60d512c542cce38d172965807535138bc97f1
unknown <smruti.parida@gmail.com>
2014-06-02 10:25:34 +0530

    leaderboard code

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -58,7 +58,7 @@ class ReportsController < ApplicationController
   def snippet
   	reports = Report.where(:aspect_id => params[:id]).order('q_asked DESC').limit(5)
   	report_data = []
-  	report_data.each {|report| report_data.push({:name => report.name, :score => report.score})}
+  	reports.each {|report| report_data.push({:name => report.name, :score => report.score})}
 	respond_to do |format|
       format.json do
         render :json => report_data.to_json
