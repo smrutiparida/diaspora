f91a69a3f24523ede1f27d0a549eb84a6a2e6220
unknown <smruti.parida@gmail.com>
2014-06-02 12:45:39 +0530

    bug posts are not getting federated

--- app/models/post.rb
+++ app/models/post.rb
@@ -122,7 +122,7 @@ class Post < ActiveRecord::Base
   def self.diaspora_initialize(params)
     new_post = self.new params.to_hash.stringify_keys.slice(*self.column_names)
     new_post.author = params[:author]
-    new_post.public = true #params[:public] if params[:public]
+    new_post.public = params[:public] if params[:public]
     new_post.pending = params[:pending] if params[:pending]
     new_post.diaspora_handle = new_post.author.diaspora_handle
     #Rails.logger.info(new_post.to_json)
