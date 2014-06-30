dd851662849210d9f144e3db0ebf8d23c2005aa5
unknown <smruti.parida@gmail.com>
2014-06-30 20:43:30 +0530

    Enabling likes for comments in diaspora

--- app/presenters/comment_presenter.rb
+++ app/presenters/comment_presenter.rb
@@ -12,7 +12,7 @@ class CommentPresenter < BasePresenter
       :created_at => @comment.created_at,
       :is_endorsed => @comment.is_endorsed,
       :likes_count => @comment.likes_count,
-      :likes => @comment.compact,
+      :likes => @comment.user_like.compact,
     }
   end
 
