019aa22d753f34d554f433e7c48b9f11ba6e458a
unknown <smruti.parida@gmail.com>
2014-07-28 13:16:00 +0530

    Sending welcome email post registration

--- app/mailers/notification_mailers/welcome_email.rb
+++ app/mailers/notification_mailers/welcome_email.rb
@@ -1,7 +1,7 @@
 module NotificationMailers
   class WelcomeEmail < NotificationMailers::Base
     def set_headers
-      @headers[:to] = name_and_address(@recipient.first_name, @recipient.unconfirmed_email)
+      @headers[:to] = name_and_address(@recipient.first_name, @recipient.email)
       @headers[:subject] = I18n.t('notifier.welcome_email.subject')
     end
   end
